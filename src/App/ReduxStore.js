31dfcf6c93fca3159e0f718e41a9807fc21fe8e2
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2024-10-02 07:13:53 +0530

    feat: Implement Redux Authentication Flow

--- src/App/ReduxStore.js
+++ src/App/ReduxStore.js
@@ -1,7 +1,7 @@
-import {configure} from '@reduxjs/toolkit'
+import {configureStore} from '@reduxjs/toolkit'
 import authReducer from './slices/authSlice'
 
-export const store = configure({
+export const store = configureStore({
     reducer: {
         auth: authReducer,
     },
