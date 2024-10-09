fce61e48f4668b6b278fe9b02e8d7e4462248b8f
Vishal Yadav <merndevvishal@gmail.com>
2024-10-09 16:28:38 +0530

    Sharable Link and Time zone component is done @vishal

--- src/Components/SharableLink.jsx
+++ src/Components/SharableLink.jsx
@@ -1,13 +1,95 @@
-import { Grid , Image , Text , Box} from "@chakra-ui/react"
-import { useSelector } from "react-redux"
-const SharableLink = () => {
-  const sharableLink = useSelector((state)=>state.slice.UserInfo.sharableLink);
-  // this holds a sharable link display it
-  return (
-    <Grid  w='100%'>
- 
-    </Grid>
-  )
-}
+import { Grid, Text, Box, Flex, Button, Center } from "@chakra-ui/react";
 
-export default SharableLink
+import { useState } from "react";
+import { useSelector } from "react-redux";
+const SharableLink = () => {
+  // this holds a sharable link display it
+  const sharableLink = useSelector(
+    (state) => state.slice.UserInfo.sharableLink
+  );
+
+  const [copied, setcopied] = useState(false);
+
+  // COPY BUTTON CLICK FUNCION
+  const handleClick = () => {
+    setcopied(true);
+    setTimeout(() => {
+      setcopied(false);
+    }, 5000);
+  };
+
+  return (
+    <Grid
+      placeItems={"center "}
+      py={30}
+      px={10}
+      alignItems={"flex-end"}
+      h={"100vh"}
+    >
+      <Flex gap={10} flexDirection={"column"} alignItems={"center"}>
+        <Box>
+          <Flex
+            fontFamily={"sans-serif"}
+            fontSize={{
+              base: "20px",
+              sm: "22px",
+              md: "28px",
+              lg: "32px",
+            }}
+            fontWeight={"bold"}
+            columnGap={5}
+            mb={7}
+            flexDirection={{ base: "column", lg: "row" }}
+          >
+            <Text>Your Sharable Link is: </Text>
+
+            <Text flex={1} color="#4C9FEB">
+              {sharableLink}
+            </Text>
+          </Flex>
+          <Center>
+            <Button
+              fontFamily={"sans-serif"}
+              fontSize={{
+                base: "12px",
+                sm: "13px",
+                md: "15px",
+                lg: "17px",
+              }}
+              fontWeight={"600"}
+              w={"136px"}
+              color={"white"}
+              py={"10px"}
+              borderRadius={"md"}
+              bg={copied ? "#00E600" : "#4C9FEB"}
+              onClick={handleClick}
+            >
+              {copied ? "Copied" : "Copy link"}
+            </Button>
+          </Center>
+        </Box>
+      </Flex>
+      <Box
+        textAlign={"center"}
+        width={{ base: "300px", md: "500px", lg: "610px" }}
+        p={7}
+        bg={"#9D9FA2"}
+        color={"white"}
+        borderRadius={"md"}
+        fontFamily={"sans-serif"}
+        fontSize={{
+          base: "12px",
+          sm: "13px",
+          md: "15px",
+          lg: "15px",
+        }}
+        fontWeight={"600"}
+      >
+        with this link anyone able to book one to one session with you on your
+        given availability date
+      </Box>
+    </Grid>
+  );
+};
+
+export default SharableLink;
--- src/Components/TimeZone.jsx
+++ src/Components/TimeZone.jsx
@@ -1,15 +1,58 @@
-import { useSelector} from "react-redux"
+import { Box, Center, Flex, Grid, HStack, Text } from "@chakra-ui/react";
+import { useSelector } from "react-redux";
 
 const TimeZone = () => {
-   
- // in this component users time zone should be visible
- const userTimeZone = useSelector((state)=>state.slice.UserInfo.timeZone)
+  // in this component users time zone should be visible
+  const userTimeZone = useSelector((state) => state.slice.UserInfo.timeZone);
 
   return (
-   <>
-   
-   </>
-  )
-}
+    <>
+      <Grid
+        h={"100vh"}
+        placeItems={"center"}
+        alignItems={"flex-end"}
+        py={30}
+        px={10}
+      >
+        <Flex flexDirection={"column"}>
+          <Center
+            fontFamily={"sans-serif"}
+            fontSize={{
+              base: "20px",
+              sm: "22px",
+              md: "28px",
+              lg: "32px",
+            }}
+            fontWeight={"bold"}
+            alignItems={{ base: "center", lg: "flex-end" }}
+            flexDirection={{ base: "column", md: "row" }}
+          >
+            <Text mr={5}>Your Detected TimeZone is:</Text>
+            <Text color={"#4C9FEB"}>{userTimeZone}</Text>
+          </Center>
+        </Flex>
+        <Center
+          textAlign={"center"}
+          w={{ base: "300px", sm: "400px", md: "500px", lg: "610px" }}
+          p={7}
+          bg={"#9D9FA2"}
+          color={"white"}
+          borderRadius={"md"}
+          fontFamily={"sans-serif"}
+          fontSize={{
+            base: "12px",
+            sm: "13px",
+            md: "15px",
+            lg: "15px",
+          }}
+          fontWeight={"600"}
+        >
+          Time zone will affect Availability slots on time of UTC time
+          Conversion , from your time time zone to Booker timezone
+        </Center>
+      </Grid>
+    </>
+  );
+};
 
-export default TimeZone
+export default TimeZone;
