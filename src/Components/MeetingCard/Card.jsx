18483933bc39eae28674b92e7a8f046692150df6
Vishal Yadav <merndevvishal@gmail.com>
2024-10-14 16:18:09 +0530

    ViewMeetings component is fixed @vishal

---
+++ src/Components/MeetingCard/Card.jsx
@@ -0,0 +1,70 @@
+import { Box, Button, Flex, Text } from "@chakra-ui/react";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Card = ({ data }) => {
+  const formattedDate = new Date(data.sessionInfo.StartDateAndTime)
+    .toISOString()
+    .split("T")[0]
+    .replace(/-/g, "/");
+
+  const formattedTime = new Date(
+    data.sessionInfo.StartDateAndTime
+  ).toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+
+    hour12: true,
+  });
+
+  console.log("orignal:", data.sessionInfo.StartDateAndTime);
+  console.log("time:", formattedTime);
+  console.log("date:", formattedDate);
+
+  return (
+    <Flex
+      flexDirection={"column"}
+      borderTop={"12px solid #4C9FEB"}
+      borderX={"1px solid"}
+      borderBottom={"1px solid"}
+      rounded={"md"}
+      p={{ base: 2, md: 5 }}
+      gap={5}
+      minW={{ base: "17rem", sm: "18rem", md: "24rem" }}
+      fontFamily={"sans-serif"}
+      fontSize={{
+        base: "14px",
+        sm: "16px",
+        md: "18px",
+        lg: "20px",
+      }}
+      fontWeight={"500"}
+      boxShadow={"0px 0px 5px 1px rgba(0,0,0,0.47);"}
+    >
+      <Box>
+        Session with{" "}
+        <Text display={"inline-block"} color={"#4C9FEB"}>
+          {data.bookerInfo.bookerName}
+        </Text>
+      </Box>
+      <Text>Date - {formattedDate}</Text>
+      <Text>Time - {formattedTime}</Text>
+
+      <Button
+        w={"8rem"}
+        color={"white"}
+        bg={"#4C9FEB"}
+        py={2}
+        px={9}
+        rounded={"md"}
+        alignSelf={"end"}
+      >
+        <Link to={data.sessionInfo.meetingLink} target="_blank">
+          Join
+        </Link>
+      </Button>
+    </Flex>
+  );
+};
+
+export default Card;
--- src/Components/ViewMeetings.jsx
+++ src/Components/ViewMeetings.jsx
@@ -1,76 +1,135 @@
-import { useEffect , useState , useMemo} from "react"
-import { Text } from "@chakra-ui/react";
+import { useEffect, useState, useRef } from "react";
+import { Flex, Text } from "@chakra-ui/react";
 import { Grid } from "@chakra-ui/react";
-import { displayLoadingModal , hideLoadingModal } from "../App/Slices/MainSlice";
+import { displayLoadingModal, hideLoadingModal } from "../App/Slices/MainSlice";
 import { useDispatch } from "react-redux";
-
+import Card from "./MeetingCard/Card";
 
 const ViewMeetings = () => {
-
-  const [sessions,setSessions] = useState([]);
-  const [noSessions,setNoSessions] = useState('');
+  const [sessions, setSessions] = useState([]);
+  const [noSessions, setNoSessions] = useState("");
   const dispatch = useDispatch();
 
   const filterUpcomingSessionsOnly = (events) => {
-    const filtered = events.filter((i)=>{
+    const filtered = events.filter((i) => {
       const date = new Date(i.sessionInfo.StartDateAndTime);
-      const  today = new Date();
+      const today = new Date();
       if (today < date) {
         return i;
       }
-    })
-    if(filtered.length == 0) {
-      setNoSessions('No Upcoming sessions')
+    });
+    if (filtered.length == 0) {
+      setNoSessions("No Upcoming sessions");
     }
-    setSessions(filtered); // set only upcoming session for host 
-  } // function for filtering only upcoming sessions 
-  console.log(sessions)
+    setSessions(filtered); // set only upcoming session for host
+  }; // function for filtering only upcoming sessions
 
-  const fetchUpcomingSessions = async() => {
+  console.log(sessions);
+
+  const fetchUpcomingSessions = async () => {
     dispatch(displayLoadingModal()); // display loading modal
 
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     const url = import.meta.env.VITE_FETCH_SESSIONS;
 
     try {
-      const response = await fetch(url,{
-        headers : {
-          'Authorization' : `Bearer ${token}`
-        }
-      },{});
-      let event = []
-      const {events} = await response.json();
+      const response = await fetch(
+        url,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+        {}
+      );
+      let event = [];
+      const { events } = await response.json();
       if (events != null || events != undefined) {
-        event = [...events]
+        event = [...events];
       }
       if (event.length > 0) {
-         console.log('yes')
-         filterUpcomingSessionsOnly(event);
-         dispatch(hideLoadingModal());
-      }else {
+        console.log("yes");
+        filterUpcomingSessionsOnly(event);
         dispatch(hideLoadingModal());
-        setNoSessions('No Upcoming Sessions Found');
-
+      } else {
+        dispatch(hideLoadingModal());
+        setNoSessions("No Upcoming Sessions Found");
       }
-    }catch(error) {
+    } catch (error) {
       dispatch(hideLoadingModal());
-      setNoSessions('Error During Fetching Upcoming Sessions')
+      setNoSessions("Error During Fetching Upcoming Sessions");
     }
-    
-  }; // fetch sessions 
+  }; // fetch sessions
 
-  useEffect(()=>{fetchUpcomingSessions()},[]);
+  useEffect(() => {
+    fetchUpcomingSessions();
+  }, []);
+
+  const scrollRef = useRef(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [scrollLeft, setScrollLeft] = useState(0);
+
+  const onMouseDown = (e) => {
+    setIsDragging(true);
+    setStartX(e.pageX - scrollRef.current.offsetLeft);
+    setScrollLeft(scrollRef.current.scrollLeft);
+  };
+
+  const onMouseLeave = () => {
+    setIsDragging(false);
+  };
+
+  const onMouseUp = () => {
+    setIsDragging(false);
+  };
+
+  const onMouseMove = (e) => {
+    if (!isDragging) return;
+    e.preventDefault();
+    const x = e.pageX - scrollRef.current.offsetLeft;
+    const walk = (x - startX) * 2; // The "2" is for scroll speed
+    scrollRef.current.scrollLeft = scrollLeft - walk;
+  };
 
   return (
-    <Grid mt={20} border='1px solid red' placeItems='center'>
-     <Text>{noSessions}</Text> {/** this text is important */}
-      {
-         sessions.map((i,index)=>(
-          <p key={index}>{i.bookerInfo.bookerName}</p> // example
-        ))
-      }
-    </Grid>
-  )
-}
+    <>
+      <Grid
+        minHeight={"100vh"}
+        placeItems={"center"}
+        gridAutoFlow={{ base: "row", md: "column" }}
+        userSelect={"none"}
+        justifyContent={{ base: "normal", md: "center" }}
+        py={{ base: 10, md: 0 }}
+      >
+        <Text justifySelf={"center"} fontSize={"24px"} fontWeight={"500"}>
+          {noSessions}
+        </Text>
+        <Flex
+          ref={scrollRef}
+          onMouseDown={onMouseDown}
+          onMouseLeave={onMouseLeave}
+          onMouseUp={onMouseUp}
+          onMouseMove={onMouseMove}
+          _active={{ cursor: "grabbing" }}
+          flexDir={{ base: "column", md: "row" }}
+          width={"min(82%, 100%)"}
+          gap={10}
+          style={{
+            whiteSpace: "nowrap",
+            overflowX: "auto",
+            overflowY: "hidden",
+            scrollbarWidth: "thin",
+          }}
+          py={10}
+        >
+          {sessions.map((i, index) => (
+            <Card key={index} data={i} />
+          ))}
+        </Flex>
+      </Grid>
+    </>
+  );
+};
 
-export default ViewMeetings
+export default ViewMeetings;
